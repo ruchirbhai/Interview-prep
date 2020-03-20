cc37f210134b2b859245d80daa300b47cf885780
ruchirbhai <ruchirbhai@gmail.com>
2020-03-19 17:46:20 -0700

    updates

--- CoinChange.py
+++ CoinChange.py
@@ -10,7 +10,7 @@ def num_coins(cents):
     if cents < 1:
         return 0
 
-    coins = [25,10,5,1]
+    coins = [25,10,1]
     coin_number = 0
     for coin in coins:
         coin_number += int(cents/coin)
@@ -20,5 +20,29 @@ def num_coins(cents):
 
     return coin_number
 
+
+def min_coins(cents):
+    if cents < 1:
+        return 0
+    # get the answer for the min coins when you have no nickels
+    coins = [25,10,1]
+    coin_number = 0
+
+    mod_25 = cents % 25
+    mod_10 = cents % 10
+
+    if mod_25 >= 10:
+        coin_number += int(cents / 10)
+        coin_number += mod_10
+    else:
+        coin_number += int(cents / 25)
+        coin_number += mod_25
+
+    return coin_number
+
+
 if __name__ == '__main__':
-    print(num_coins(cents=int(sys.argv[1])))
+    greedy = num_coins(cents=int(sys.argv[1]))
+    mini = min_coins(cents=int(sys.argv[1]))
+    print('greedy=', greedy, 'and mini =', mini)
+
